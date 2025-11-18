a8efb9a54c84235b99788a8d3d094d663e2db79d
robert <robertudrea2002@yahoo.ro>
2025-11-18 22:51:38 +0200

    small py recap

---
+++ python_crash_course/main.py
@@ -0,0 +1,30 @@
+#Data Types
+# Int - 123
+# Float - 123.0
+# String - "hello"
+# Boolean - True, False
+
+print("Hello")
+
+# Input
+a = 23
+name = input("Name= ")
+print(name)
+
+# Math
+divide = 9 / 3 # 3.0
+divide = int(9 / 3) # 3
+divide = 10 // 3 # 3, similar cu Math.floor()
+
+# String methods
+h = "hello"
+upper_hello = h.upper()
+h.find("e") # return index or -1
+h.index("f") # return index or ValueError
+
+age = 36
+txt = f"My name is John, I am {age}"
+
+crazy = "hh" * 3
+
+# min 27:15
