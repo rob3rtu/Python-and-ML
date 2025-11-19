6f49a5c50a09cfc639d0dc1cd6f98e18e0aa4e4a
robert <robertudrea2002@yahoo.ro>
2025-11-19 11:57:26 +0200

    Colections practice

--- python_crash_course/main.py
+++ python_crash_course/main.py
@@ -8,8 +8,8 @@ print("Hello")
 
 # Input
 a = 23
-name = input("Name= ")
-print(name)
+# name = input("Name= ")
+# print(name)
 
 # Math
 divide = 9 / 3 # 3.0
@@ -20,11 +20,55 @@ divide = 10 // 3 # 3, similar cu Math.floor()
 h = "hello"
 upper_hello = h.upper()
 h.find("e") # return index or -1
-h.index("f") # return index or ValueError
+h.index("l") # return index or ValueError
 
 age = 36
 txt = f"My name is John, I am {age}"
 
 crazy = "hh" * 3
 
-# min 27:15
+x = 1 and 2 or 4 or (not 3)
+
+# Array and Tuple 
+
+x = [2, 3, 4, 5, 6]
+x.append(1)
+last = x.pop()
+fist = x.pop(0)
+
+copy = x[:]
+
+t = (1, 3, 2, 5) # tuple, imutable list
+
+for i in range(10):
+    print(i, end=" ")
+print()
+
+for i in [1, 2, 3, 4, 5]:
+    print(i, end=" ")
+print()
+
+for i, element in enumerate([1, 2, 3, 4, 5]):
+    print(i, element, end=", ")
+print()
+
+reverse_list = x[::-1]
+
+# Set
+x = set()
+x.add(5)
+x.add(2)
+x.remove(5)
+exist = 4 in x # not in
+
+# Dictionaire (HashMap)
+x = {} # {'key': value}
+x['key'] = 1
+exist = 'key' in x
+
+l = list(x.values())
+del x['key']
+
+for key, value in x.values():
+    print(key, value)
+
